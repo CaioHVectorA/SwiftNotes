8f196f42f28a06bf656f48224eb9902b5e21abce
Caio Henrique <109510970+CaioHVectorA@users.noreply.github.com>
2023-08-02 02:18:28 +0000

    Update 4 files

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,6 +1,12 @@
 export type Note = {
-    title: string,
-    body: string,
-    created: Date,
-    modified: Date,
-}
+  title: string;
+  body: string;
+  created: Date;
+  modified: Date;
+};
+// export type Note = {
+//   title: string;
+//   body: React.ReactNode[];
+//   created: Date;
+//   modified: Date;
+// };
